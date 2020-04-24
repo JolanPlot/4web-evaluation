4da2ff6040690561f4cf6461d53b7a8b7a78eed3
Jolan Plot <jolan.plot@hotmail.fr>
2020-04-24 13:15:10 +0200

    form login

---
+++ src/Login/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Login.css";
+
+const Login = ({
+    email,
+    setEmail,
+    password,
+    setPassword,
+    formHandler,
+    hideErrorMessage
+}) => (
+        <>
+            <div className="login">
+                <form className="login" onSubmit={formHandler}>
+                    <label htmlFor="email">Login</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        className="login_email"
+                        placeholder="Email"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                        className="login_password"
+                        placeholder="Password"
+                    />
+                    <h3 className="login_error " style={hideErrorMessage}>
+                        Bad credentials
+          </h3>
+                    <button className="login_button">Login</button>
+                </form>
+            </div>
+        </>
+    );
+
+export default Login;
