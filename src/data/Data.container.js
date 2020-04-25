9ad312540410bfd5bd36f9d5e901e40b0c356311
Jolan Plot <jolan.plot@hotmail.fr>
2020-04-25 15:07:41 +0200

    import API data

--- src/data/Data.container.js
+++ src/data/Data.container.js
@@ -4,19 +4,38 @@ import Array from "./Data"
 import axios from 'axios'
 
 const EnchanceData = props => {
-    const [itemspage, setitemsPage] = useState(10);
-    const [items, setItems] = useState("")
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10);
 
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            const res = await axios.get('https://my.api.mockaroo.com/collectionbd.json?key=007a70e0');
+            setPosts(res.data);
+            setLoading(false);
+        };
+
+        fetchPosts();
+    }, []);
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
     return (
-        <Array {...props}
-            itemspage={itemspage}
-            setitemsPage={setitemsPage}
-            items={items}
-            setItems={setItems}
-
-        />
+        <div className='container mt-5'>
+            <h1 className='text-primary mb-3'>Collection</h1>
+            <Posts posts={currentPosts} loading={loading} />
+            <Pagination
+                postsPerPage={postsPerPage}
+                totalPosts={posts.length}
+                paginate={paginate}
+            />
+        </div>
     );
+
 
 }
 
