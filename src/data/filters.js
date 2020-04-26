adcf1cf56944796f221a4a482969fb507c98df9d
Jolan Plot <jolan.plot@hotmail.fr>
2020-04-26 14:02:50 +0200

    add text search

--- src/data/filters.js
+++ src/data/filters.js
@@ -13,6 +13,16 @@ const filters = ({ postsPerPage, setPostsPerPage, change_nb_posts }) => {
                 />
                 <button className="button_nb_posts" onClick={change_nb_posts} > Change </button>
             </div>
+            <div className="search">
+                <label for="text_search" >Votre recherche : </label>
+                <input type="text" value=""
+
+                />
+                <button className="button_search" > Rechercher </button>
+            </div>
+
+
+
             <div className="radio_button" >
                 <label for="Dedicaces">Dédicacés ? </label>
                 <label for="Oui" >Oui</label>
